8457ad78051a37cde69ef4f53cd85bcf8f7dca68
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-02-09 16:05:43 -0300

    Configuração do Redux feita.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import reduxStore from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={ reduxStore }>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
---
+++ src/redux/reducers/user.js
@@ -0,0 +1,14 @@
+// Esse reducer será responsável por tratar as informações da pessoa usuária.replace(/[^0-9,]*/g, '').replace(',', '.')
+
+const INITIAL_STATE = {
+  email: '',
+}
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default user;
