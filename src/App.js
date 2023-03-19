4686cbaca6b1035c1554526450c39a6a20f259ad
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-03-19 00:08:39 -0300

    refactor(src/App.js): Alteração não relevante feita para teste no "gh-pages"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
